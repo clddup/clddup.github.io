2661ce5c85237551dd4250e0e2e6eca80be38047
clddup <wcl0749@gmail.com>
2025-01-23 18:09:36 +0800

    feat: 增加鼠标动效

---
+++ src/components/CursorEffects.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { fairyDustCursor } from "cursor-effects";
+import { onMounted } from "vue";
+onMounted(() => {
+  new fairyDustCursor();
+});
+</script>
+<template></template>
